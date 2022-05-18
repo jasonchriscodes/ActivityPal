27e9311b7623a3170e10ab0adaf6dd93503ca975
mrb34n135421234 <jasonchristian1234@gmail.com>
2022-05-18 18:28:11 +1200

    Adding application layer pagination logic

--- Social_Activity_Planner/Application/Activities/List.cs
+++ Social_Activity_Planner/Application/Activities/List.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using Application.Core;
@@ -11,31 +12,37 @@ using Persistence;
 
 namespace Application.Activities
 {
-    public class List
-    {
-        public class Query : IRequest<Result<List<ActivityDto>>> { }
+ public class List
+ {
+  public class Query : IRequest<Result<PagedList<ActivityDto>>>
+  {
+   public PagingParams Params { get; set; }
+  }
 
-        public class Handler : IRequestHandler<Query, Result<List<ActivityDto>>>
-        {
-            private readonly DataContext _context;
-            private readonly IMapper _mapper;
-            private readonly IUserAccessor _userAccessor;
-            public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
-            {
-                _userAccessor = userAccessor;
-                _mapper = mapper;
-                _context = context;
-            }
+  public class Handler : IRequestHandler<Query, Result<PagedList<ActivityDto>>>
+  {
+   private readonly DataContext _context;
+   private readonly IMapper _mapper;
+   private readonly IUserAccessor _userAccessor;
+   public Handler(DataContext context, IMapper mapper, IUserAccessor userAccessor)
+   {
+    _userAccessor = userAccessor;
+    _mapper = mapper;
+    _context = context;
+   }
 
-            public async Task<Result<List<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
-            {
-                var activities = await _context.Activities
-                    .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider, 
-                        new {currentUsername = _userAccessor.GetUsername()})
-                    .ToListAsync(cancellationToken);
+   public async Task<Result<PagedList<ActivityDto>>> Handle(Query request, CancellationToken cancellationToken)
+   {
+    var query = _context.Activities
+        .ProjectTo<ActivityDto>(_mapper.ConfigurationProvider,
+            new { currentUsername = _userAccessor.GetUsername() })
+        .AsQueryable();
 
-                return Result<List<ActivityDto>>.Success(activities);
-            }
-        }
-    }
+    return Result<PagedList<ActivityDto>>.Success(
+        await PagedList<ActivityDto>.CreateAsync(query, request.Params.PageNumber,
+        request.Params.PageSize)
+    );
+   }
+  }
+ }
 }
